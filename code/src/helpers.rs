af95964d372c78bbb4c6ffd6c5c75bcd17b08ad8
cmllr <mail@chmr.eu>
2024-01-31 19:13:04 +0100

    Add license

--- code/src/helpers.rs
+++ code/src/helpers.rs
@@ -1,3 +1,22 @@
+/*
+spargel - THE blog engine nobody asked for!
+Copyright (C) 2024  Christoph Mueller
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
 use std::io;
 use std::fs;
 use std::os::unix::ffi::OsStrExt;
